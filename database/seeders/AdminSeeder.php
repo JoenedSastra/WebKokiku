15da4aa0ec69122b3330940bb84df699778bd928
JoenedSastra <joenedsastra812@gmail.com>
2026-07-10 19:43:18 +0700

    gmail admin

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,10 +11,10 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'kokiku@email.com'],
+            ['email' => 'kokiku@gmail.com'],
             [
                 'name' => 'Administrator',
-                'email' => 'kokiku@email.com',
+                'email' => 'kokiku@gmail.com',
                 'password' => Hash::make('dellarahmatjs12'),
                 'role' => 'admin',
             ]
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,15 +74,24 @@
         }
 
         .icon-button svg,
-        .input-group-text svg {
+        .input-group-text svg,
+        .input-group-text .icon-emoji {
             width: 18px;
             height: 18px;
-            display: block;
-            stroke: currentColor;
-            stroke-width: 1.2;
-            fill: none;
-            stroke-linecap: round;
-            stroke-linejoin: round;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .input-group-text .icon-emoji {
+            font-size: 16px;
+            line-height: 1;
+        }
+
+        /* force a thinner stroke for selected icons */
+        .input-group-text svg.thin,
+        .icon-button svg.thin {
+            stroke-width: 0.9 !important;
         }
 
         .eye-animation {
@@ -150,9 +159,7 @@ Login
 
 <div class="input-group">
     <span class="input-group-text">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" stroke="currentColor" fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.708 2.827L15 12.383V5.383zm-.034 7.166L9.2 8.82 8 9.583 6.8 8.82 1.034 12.55A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.451z"/>
-        </svg>
+        <span class="icon-emoji">📧</span>
     </span>
     <input type="email"
     class="form-control"
@@ -168,9 +175,7 @@ Login
 
 <div class="input-group">
     <span class="input-group-text">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" stroke="currentColor" fill="none" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M8 1a4 4 0 0 0-4 4v3H3a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1h-1V5a4 4 0 0 0-4-4zm-2 4a2 2 0 1 1 4 0v3H6V5zm-2 4h8v5H4v-5z"/>
-        </svg>
+        <span class="icon-emoji">🔒</span>
     </span>
     <input
     type="password"
